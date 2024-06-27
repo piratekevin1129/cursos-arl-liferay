4a543dae59e6d627299112faee37dae3095063aa
Kevin Gaviria <kevingaviria.1994@gmail.com>
2024-06-27 08:30:34 -0500

    version 1.0

---
+++ cursos-arl/scripts/seccion2.js
@@ -0,0 +1,11 @@
+function clickNextPaginator(btn){
+    var paginator_parent = btn.parentNode
+    var wrap = paginator_parent.getAttribute('wrap')
+    var actual_page = paginator_parent.getAttribute('page')
+    
+    if(actual_page)
+}
+
+function clickPrevPaginator(btn){
+
+}
