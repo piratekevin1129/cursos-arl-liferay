67697d54b647ea3b3da4f759f74db3a31d9905ff
Kevin Gaviria <kevingaviria.1994@gmail.com>
2024-06-27 15:57:14 -0500

    version 1.2

--- cursos-arl/scripts/section2-slider.js
+++ cursos-arl/scripts/section2-slider.js
@@ -1,3 +1,5 @@
+console.log("ready! seccion 2 - slider")
+
 function clickNextPaginator(btn){
     var paginator_parent = btn.parentNode
     var pages_content = paginator_parent.getElementsByTagName('div')[0].getElementsByTagName('div')
@@ -5,12 +7,14 @@ function clickNextPaginator(btn){
     var wrap = document.getElementById(paginator_parent.getAttribute('wrap'))
     var actual_page = wrap.getAttribute('page')
     var total_pages = wrap.getAttribute('items')
-    var seccion_page = wrap.getAttribute('seccion')
-    var ancho_page = wrap.getAttribute('ancho')
+    var seccion = parseInt(wrap.getAttribute('seccion'))
+    var ancho = parseInt(wrap.getAttribute('ancho'))
+    var espacio = parseInt(wrap.getAttribute('espacio'))
+
     var wrapper = document.getElementById(paginator_parent.getAttribute('wrapper'))
 
-    if(actual_page<=(total_pages-seccion_page)){
-        var wrapper_left = (actual_page*ancho_page) + (actual_page*15)
+    if(actual_page<=(total_pages-seccion)){
+        var wrapper_left = actual_page * (ancho + (espacio * 2))
         wrapper.style.left = (0-wrapper_left)+'px'
 
         actual_page++
@@ -22,14 +26,19 @@ function clickNextPaginator(btn){
         pages_content[actual_page-1].innerHTML = '<span></span>'
     }
 
+    var btn_next = (btn)
+    var btn_prev = paginator_parent.getElementsByTagName('button')[0]
 
-    if(actual_page==(total_pages-(seccion_page-1))){
+    if(actual_page==(total_pages-(seccion-1))){
+        //llegó al final
         right_shadow.style.display = 'none'
+        btn_next.className = 'seccion-2-paginator-btn-locked'
     }else{
         right_shadow.style.display = 'block'
     }
     if(actual_page>1){
         left_shadow.style.display = 'block'
+        btn_prev.className = ''
     }else{
         left_shadow.style.display = 'none'
     }
@@ -42,15 +51,17 @@ function clickPrevPaginator(btn){
     var wrap = document.getElementById(paginator_parent.getAttribute('wrap'))
     var actual_page = wrap.getAttribute('page')
     var total_pages = wrap.getAttribute('items')
-    var seccion_page = wrap.getAttribute('seccion')
-    var ancho_page = wrap.getAttribute('ancho')
+    var seccion = parseInt(wrap.getAttribute('seccion'))
+    var ancho = parseInt(wrap.getAttribute('ancho'))
+    var espacio = parseInt(wrap.getAttribute('espacio'))
+
     var wrapper = document.getElementById(paginator_parent.getAttribute('wrapper'))
 
     if(actual_page>1){
         actual_page--
         wrap.setAttribute('page',actual_page)
 
-        var wrapper_left = ((actual_page-1)*ancho_page) + ((actual_page-1)*15)
+        var wrapper_left = ((actual_page - 1) * (ancho + (espacio * 2)))
         wrapper.style.left = (0-wrapper_left)+'px'
 
         for(var i = 0;i<pages_content.length;i++){
@@ -59,16 +70,22 @@ function clickPrevPaginator(btn){
         pages_content[actual_page-1].innerHTML = '<span></span>'
     }
 
-    if(actual_page<(total_pages-(seccion_page-1))){
+    var btn_next = paginator_parent.getElementsByTagName('button')[1]
+    var btn_prev = (btn)
+
+    if(actual_page<(total_pages-(seccion-1))){
         right_shadow.style.display = 'block'
     }else{
         right_shadow.style.display = 'none'
     }
 
     if(actual_page==1){
+        //llegó al inicio
         left_shadow.style.display = 'none'
+        btn_prev.className = 'seccion-2-paginator-btn-locked'
     }else{
         left_shadow.style.display = 'block'
+        btn_next.className = ''
     }
 
 }
@@ -85,19 +102,22 @@ function setSliderCursos(params){
     var item_section = params.item_section
     var items_wrap = wrap.getElementsByClassName('seccion-2-item')
 
-    wrap_width = (item_width * item_section) + ((item_section-1) * item_space)
+    wrap_width = (item_width * item_section) + (item_section * (item_space * 2))
 
     wrap.style.width = wrap_width+'px'
     wrap.setAttribute('items',items_wrap.length)
     wrap.setAttribute('ancho',item_width)
     wrap.setAttribute('seccion',item_section)
+    wrap.setAttribute('espacio',item_space)
     
     for (i = 0;i<items_wrap.length;i++){
         items_wrap[i].style.width = item_width+'px'
-        if(i<(items_wrap.length-1)){
-            items_wrap[i].style.marginRight = item_space+'px'
-        }
+        items_wrap[i].style.marginRight = item_space+'px'
+        items_wrap[i].style.marginLeft = item_space+'px'
+    }
 
+    var limit_pages = (items_wrap.length - item_section) + 1
+    for(i = 0;i<limit_pages;i++){
         var div_page = document.createElement('div')
         if(i==0){
             var div_span = document.createElement('span')
@@ -111,7 +131,7 @@ setSliderCursos({
     wrap:'seccion-2-wrap',
     paginator:'seccion-2-paginator-pages',
     item_width:264,
-    item_space:15,
+    item_space:8,
     item_section:4
 })
 var left_shadow = document.getElementById('seccion-2-wrapper-leftshadow')
